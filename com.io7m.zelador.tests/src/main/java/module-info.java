e3daac688cbd99bd1360f1f8b6cf6b245b3eefab
Mark Raynsford <code@io7m.com>
2024-10-26 13:04:16 +0000

    Use junit BOM.

--- com.io7m.zelador.tests/src/main/java/module-info.java
+++ com.io7m.zelador.tests/src/main/java/module-info.java
@@ -31,6 +31,7 @@ open module com.io7m.zelador.tests
   requires org.junit.jupiter.engine;
   requires org.junit.platform.commons;
   requires org.junit.platform.engine;
+  requires org.junit.platform.launcher;
 
   exports com.io7m.zelador.tests;
 }
